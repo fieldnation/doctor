b387fb3f1e4cfa07cb85a58fff461cae20d7477a
Jack Spirou <jack.spirou@fieldnation.com>
2017-08-21 22:51:32 -0500

    fill in examine func

--- doctor.go
+++ doctor.go
@@ -1,23 +1,32 @@
 package doctor
 
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
 // Doctor represents a worker who will perform different
 // types of health checks periodically.
 type Doctor struct {
-	opts  options
-	appts []appt
+	appts     []appt
+	examining bool
 }
 
 // Options takes an option and returns an error.
 type Options func(*options) error
 
 type options struct {
-	ttl      int
-	interval int
+	ttl      time.Duration
+	interval time.Duration
 }
 
 type appt struct {
-	healthCheck  HealthCheck
-	opts         options
+	healthCheck HealthCheck
+	opts        options
+
+	mu           sync.RWMutex
 	billOfHealth []byte
 }
 
@@ -27,15 +36,15 @@ func New() *Doctor {
 }
 
 // TTL sets the Time to Live option value.
-func TTL(ttl int) Options {
+func TTL(ttl time.Duration) Options {
 	return func(o *options) error {
 		o.ttl = ttl
 		return nil
 	}
 }
 
-// Interval sets the Interval option value.
-func Interval(interval int) Options {
+// Regularity sets the duration of how often the health check is executed.
+func Regularity(interval time.Duration) Options {
 	return func(o *options) error {
 		o.interval = interval
 		return nil
@@ -46,12 +55,20 @@ func Interval(interval int) Options {
 type HealthCheck func() (body []byte, contentType string, err error)
 
 // Schedule a health check with some options, bascially a doctor appointment.
-func (d *Doctor) Schedule(h HealthCheck, opts ...Options) {
+func (d *Doctor) Schedule(h HealthCheck, opts ...Options) error {
+
+	// check if an examination is already underway,
+	// if so do not allow further scheduling, dynamic
+	// scheduling is not yet supported
+	if d.examining {
+		return errors.New("you can only schedule health checks before the docker begins examinations")
+	}
 
 	// create a new appointment
 	a := appt{
 		healthCheck:  h,
 		billOfHealth: []byte("{\"report\": \"no health check results\""),
+		opts:         options{interval: 5 * time.Second},
 	}
 
 	// set the request options on that appointment
@@ -61,9 +78,53 @@ func (d *Doctor) Schedule(h HealthCheck, opts ...Options) {
 
 	// append the appointment to the doctors list
 	d.appts = append(d.appts, a)
+
+	return nil
 }
 
 // Examine starts the series of health checks that were registered.
 func (d *Doctor) Examine() (<-chan bool, error) {
+
+	// officially start examinations, if we ever support
+	// dynamic/concurrent scheduling and examining this
+	// would be removed
+	d.examining = true
+
+	// range over each appointment
+	for k := range d.appts {
+
+		// set a new ticker based on the scheduled regularity
+		ticker := time.NewTicker(d.appts[k].opts.interval)
+		quit := make(chan struct{})
+
+		// execute the appointment in a seperate goroutine
+		go func() {
+			for {
+				select {
+				case <-ticker.C:
+					go func() {
+						d.appts[k].healthCheck()
+					}()
+				case <-quit:
+					ticker.Stop()
+					fmt.Println("Stopped the ticker!")
+					return
+				}
+			}
+		}()
+
+		// if there is a TTL set, close the appointment at that time
+		if d.appts[k].opts.ttl > 0 {
+
+			// it is acceptable to range over each appointment sequencially
+			// for setup, but TTL requires a goroutine to keep things moving
+			go func() {
+				time.Sleep(d.appts[k].opts.ttl)
+				close(quit)
+			}()
+		}
+
+	}
+
 	return nil, nil
 }
