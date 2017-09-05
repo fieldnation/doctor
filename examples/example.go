bac1377628af59e5a43e3db5bb0a5637dcd2b41f
Jack Spirou <jack.spirou@fieldnation.com>
2017-09-04 20:43:32 -0500

    add comments to the example

--- examples/example.go
+++ examples/example.go
@@ -8,26 +8,34 @@ import (
 )
 
 func main() {
+
+	// create new doctor
 	doc := doctor.New()
 
+	// schedule an appointment that occurs every second for 5 seconds
 	doc.Schedule(doctor.Appointment{
 		Name:        "ping",
 		HealthCheck: ping,
 	}, doctor.Regularity(1*time.Second), doctor.TTL(5*time.Second))
 
+	// schedule an appointment that occurs every second for 20 seconds
 	doc.Schedule(doctor.Appointment{
 		Name:        "pong",
 		HealthCheck: pong,
 	}, doctor.Regularity(1*time.Second), doctor.TTL(20*time.Second))
 
+	// schedule an appointment that only occurs once
 	doc.Schedule(doctor.Appointment{
 		Name:        "only once",
 		HealthCheck: onlyOnce,
 	})
 
+	// start the examination
 	ch := doc.Examine()
 
+	// slurp on the channel to recieve bills of health resulting from each health check
 	for boh := range ch {
+		// print out info on the bill of health
 		fmt.Printf("%s started at %s\n", boh.Name(), boh.Start())
 	}
 }
